2382ec2bf75fe28dacbd748438df972869af77b9
kgoldra <kgoldra@gmail.com>
2021-09-13 00:46:19 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,94 @@
+import turtle
+from shape import *
+
+mw = turtle.Screen()
+mw.title("Tetris")
+mw.bgcolor("black")
+mw.setup(width=600, height=800)
+mw.tracer(0)
+
+pen = turtle.Turtle()
+pen.penup()
+pen.speed(0)
+pen.shape("square")
+pen.setundobuffer(None)
+
+
+def draw_grid(pen, grid):
+    pen.clear()
+    starty = 230
+    startx = -110
+
+    colors = ["white",
+              "cyan",
+              "blue",
+              "orange",
+              "yellow",
+              "green",
+              "purple",
+              "red"]
+
+
+    for y in range(len(grid)):
+        for x in range(len(grid[0])):
+            screen_x = startx + (x * 20)
+            screen_y = starty - (y * 20)
+
+            color = colors[grid[y][x]]
+            pen.color(color)
+            pen.goto(screen_x, screen_y)
+            pen.stamp()
+
+
+# shape creation
+shape = Shape()
+
+#putting the shape in the grid
+grid[shape.y][shape.x] = shape.color
+
+#drawing the initial grid
+draw_grid(pen, grid)
+
+
+
+mw.listen()
+mw.onkey(lambda: shape.move_left(grid), "a")
+mw.onkey(lambda: shape.move_right(grid), "d")
+mw.onkey(lambda: shape.move_bot(grid), "s")
+
+#main game loop
+while True:
+    mw.update()
+
+    #creating new shape when there's nowhere to move below
+    if shape.y == 23 - shape.height + 1 or grid[shape.y + shape.height][shape.x] != 0 or grid[shape.y + shape.height][shape.x + shape.width] != 0:
+        shape.y = 0
+        row_clear()
+
+#something wrong below (couldn't spawn block at 0 y = 0)
+    #shape movement to the bottom
+    elif grid[shape.y + shape.height][shape.x] == 0:
+        if shape.y + 1 < 23:
+
+            for y in range(shape.height):
+                for x in range(shape.width):
+                    grid[shape.y + y][shape.x + x] = 0
+
+            shape.y += 1
+
+            for y in range(shape.height):
+                for x in range(shape.width):
+                    grid[shape.y + y][shape.x + x] = shape.color
+#            grid[shape.y][shape.x] = shape.color
+#            grid[shape.y-1][shape.x] = 0
+
+
+#        elif shape.y+1 == 23:
+#            shape.y += 1
+#            grid[shape.y][shape.x] = shape.color
+#            grid[shape.y - 1][shape.x] = 0
+
+
+    draw_grid(pen, grid)
+
+mw.mainloop()
---
+++ shape.py
@@ -0,0 +1,105 @@
+class Shape():
+    def __init__(self):
+        self.x = 5
+        self.y = 0
+        self.color = 1
+        self.shape = [[1,1],
+                      [1,1]]
+        self.height = len(self.shape)
+        self.width = len(self.shape[0])
+
+    def box(self):
+        shape = [[0, 0],
+                 [0, 0]]
+        color = 4
+
+    def move_left(self, grid):
+        if self.x > 0:
+            count = 0
+            for y in range(self.height):
+                if grid[self.y + y][self.x - 1] == 0:
+                    count += 1
+            if count == 2:
+                for y in range(self.height):
+                    for x in range(self.width):
+                        grid[self.y + y][self.x + x] = 0
+                self.x -= 1
+
+#something wrong here
+    def move_right(self, grid):
+        if self.x + self.width - 1 < 11:
+            count = 0
+            for y in range(self.height):
+                if grid[self.y + y][self.x + 1] == 0:
+                    count += 1
+            if count == 2:
+                for y in range(self.height):
+                    for x in range(self.width):
+                        grid[self.y + y][self.x + x] = 0
+                self.x += 1
+                print("moving right")
+
+
+    def move_bot(self, grid):
+        boty = 22
+        while grid[boty][self.x] != 0:
+            boty -= 1
+            print(boty)
+        grid[self.y][self.x] = 0
+        self.y = boty
+        grid[self.y][self.x] = self.color
+
+
+# function to move the block down if theres nothing below after a section is cleared
+def row_gravity():
+    for y in range(23, 0, -1):
+        count = 0
+        for x in range(12):
+            if grid[y][x] == 0:
+                count += 1
+            if count == 12:
+                for xtemp in range(12):
+                    grid[y][xtemp] = grid[y - 1][xtemp]
+                    grid[y - 1][xtemp] = 0
+
+
+# function for clearing row
+def row_clear():
+    for y in range(23, -1, -1):
+        count = 0
+        for x in grid[y]:
+            if x != 0:
+                count += 1
+            if count == 12:
+                for xtemp in range(12):
+                    grid[y][xtemp] = 0
+                row_gravity()
+
+
+
+grid = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+]
